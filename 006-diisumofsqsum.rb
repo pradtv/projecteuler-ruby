2e5d2ba79e714b8881daaa6e29d8be4653ad6439
PRADEEP TV <pradeep@wowmakers.com>
2012-09-05 11:17:26 +0530

    Sixth problem modification

--- 006-diisumofsqsum.rb
+++ 006-diisumofsqsum.rb
@@ -1,4 +1,8 @@
- puts (sqlsum = (1..100).reduce(&:+) **2) - ((1..100).map{|x| x**2}.inject(0, &:+))
- 
 
- 
+def difference
+  sum_of_squares = (1..100).map{|x| x**2}.inject(0, &:+)
+  squares_of_sums = (1..100).reduce(&:+) **2
+  squares_of_sums - sum_of_squares
+end  
+
+puts difference 
